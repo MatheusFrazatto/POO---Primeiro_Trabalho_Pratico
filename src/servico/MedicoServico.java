999788f5cd7957d003745caaee95799717e0fcee
Matheus Frazatto <matheusfrazattodias@gmail.com>
2025-11-06 11:47:02 -0300

    Ajustes e correções na classe MedicoServico

--- src/servico/MedicoServico.java
+++ src/servico/MedicoServico.java
@@ -11,19 +11,30 @@ public class MedicoServico {
     private int proximoIdMedico = 1;
     private PacienteServico pacienteServico;
 
-    public MedicoServico() {
+    public MedicoServico(PacienteServico pacienteServico) {
+        this.pacienteServico = pacienteServico;
     }
 
     public boolean atualizarDadosAdicionais(int idPaciente, boolean fuma, boolean bebe, boolean colesterol, boolean diabetes, String doencasCardiacas, String cirurgias, String alergias) {
         Paciente paciente = pacienteServico.buscarPacientePorId(idPaciente);
-        if (paciente == null) {
+        if (paciente != null) {
             DadosAdicionais dadosAdicionais = paciente.getDadosAdicionais();
             dadosAdicionais.setFuma(fuma);
             dadosAdicionais.setBebe(bebe);
             dadosAdicionais.setColesterol(colesterol);
+            dadosAdicionais.setDiabete(diabetes);
             dadosAdicionais.setDoencasCardiacas(doencasCardiacas);
             dadosAdicionais.setCirurgias(cirurgias);
             dadosAdicionais.setAlergias(alergias);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean limparDadosAdicionais(int idPaciente) {
+        Paciente paciente = pacienteServico.buscarPacientePorId(idPaciente);
+        if (paciente != null) {
+            paciente.setDadosAdicionais(new DadosAdicionais());
             return true;
         }
         return false;
@@ -33,7 +44,7 @@ public class MedicoServico {
         Paciente paciente = pacienteServico.buscarPacientePorId(idPaciente);
         Medico medico = this.buscarMedicoPorId(idMedico);
         if (paciente != null && medico != null) {
-            Prontuario novoProntuario = new Prontuario(paciente.getId(), LocalDate.now(), sintomas, diagnostico, prescricao, medico);
+            Prontuario novoProntuario = new Prontuario(0, LocalDate.now(), sintomas, diagnostico, prescricao, medico);
             paciente.adicionarProntuario(novoProntuario);
             return true;
         }
@@ -74,45 +85,55 @@ public class MedicoServico {
     public String gerarReceita(int idPaciente, int idMedico, String prescricao) {
         Paciente paciente = pacienteServico.buscarPacientePorId(idPaciente);
         Medico medico = this.buscarMedicoPorId(idMedico);
-        if (paciente != null) {
+        if (paciente == null) {
             return "ERRO: Paciente não encontrado";
         }
+        if (medico == null) {
+            return "ERRO: Médico não encontrado";
+        }
         return String.format("--- Receita Médica ---\n" +
-                "Paciente: %s\n" +
-                "CPF: %s\n" +
-                "Data: %s\n" +
-                "Ass: %s",
-                paciente.getNome(), paciente.getCpf(), prescricao, LocalDate.now(), medico);
+                        "Paciente: %s\n" +
+                        "CPF: %s\n" +
+                        "Prescrição: %s\n" +
+                        "Data: %s\n" +
+                        "Ass: %s (CRM: %s)",
+                paciente.getNome(), paciente.getCpf(), prescricao, LocalDate.now(), medico.getNome(), medico.getCrm());
     }
 
     public String gerarAtestado(int idPaciente, int idMedico, int diasAfastamento) {
         Paciente paciente = pacienteServico.buscarPacientePorId(idPaciente);
         Medico medico = this.buscarMedicoPorId(idMedico);
-        if (paciente != null) {
+        if (paciente == null) {
             return "ERRO: Paciente não encontrado";
+        }
+        if (medico == null) {
+            return "ERRO: Médico não encontrado";
         }
         return String.format("--- Atestado Médico ---\n" +
-                        "Atesto para os devido fins que paciente %s, postador(a) do CPF %s, necessita de %s dias(s) de afastamento.\n\n" +
+                        "Atesto para os devido fins que paciente %s, portador(a) do CPF %s, necessita de %d dias(s) de afastamento.\n\n" +
                         "Data: %s\n" +
-                        "Ass: %s",
-                paciente.getNome(), paciente.getCpf(), diasAfastamento, LocalDate.now(), medico);
+                        "Ass: %s (CRM: %s))",
+                paciente.getNome(), paciente.getCpf(), diasAfastamento, LocalDate.now(), medico.getNome(), medico.getCrm());
     }
 
-    public String gerarDeclaracaoAcompanhamento(int idPaciente, int idMedico, int diasAfastamento) {
+    public String gerarDeclaracaoAcompanhamento(int idPaciente, int idMedico, int diasAfastamento, String nomeAcompanhante) {
         Paciente paciente = pacienteServico.buscarPacientePorId(idPaciente);
         Medico medico = this.buscarMedicoPorId(idMedico);
-        if (paciente != null) {
+        if (paciente == null) {
             return "ERRO: Paciente não encontrado";
         }
+        if (medico == null) {
+            return "ERRO: Médico não encontrado";
+        }
         return String.format("--- Atestado Médico---\n" +
-                        "Atesto para os devido fins que o(a) Sr(a) %s, postador(a) do CPF %s, esteve acompanhando o paciente desde o dia %s.\n\n" +
+                        "Declaro para os devido fins que o(a) Sr(a) %s, esteve nesta unidade hospitalar no dia %d, acompanhando o(a) paciente %s.\n\n" +
                         "Data: %s\n" +
-                        "Ass: %s",
-                paciente.getNome(), paciente.getCpf(), diasAfastamento, LocalDate.now(), LocalDate.now(),medico);
+                        "Ass: %s (CRM: %s)",
+                nomeAcompanhante, LocalDate.now(), paciente.getNome(), LocalDate.now(), medico.getNome(), medico.getCrm());
     }
 
-    public List<Paciente> getClientesAtendidosMes (int idMedico, int mes, int ano, list<Consulta> todasConsultas) {
-        ...
+    public List<Paciente> getClientesAtendidosMes(int idMedico, int mes, int ano, list<Consulta> todasConsultas) {
+        return new ArrayList<>();
     }
 
 
